4c099c0f2b44e5d837584bf5129c854ff66ada6c
thomasop <tdss33@hotmail.com>
2023-03-25 14:17:49 +0100

    Initial Commit

---
+++ todolist/todolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  todolistApp.swift
+//  todolist
+//
+//  Created by Thomas Da Silva on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct todolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
